de6f871512ccc37a4102f1df2e11e46e3df3a1a8
Keith McHugh <mchughkeith3@gmail.com>
2022-09-29 08:56:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Multithread.ThreadingSamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NoThread.NoThreadExample();
+            Threading1.DemoThread();
+        }
+    }
+}
---
+++ ThreadingSamples/NoThread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithread.ThreadingSamples
+{
+    internal class NoThread
+    {    
+    public static void NoThreadExample()
+        {
+            NoThread1();
+            NoThread2();
+        }
+    static void NoThread1()
+    {
+        while (true)
+        {
+            Console.WriteLine("First Thread Here... Yo");
+                Thread.Sleep(500);
+        }
+    } 
+        static void NoThread2()
+    {
+        while (true)
+        {
+                Console.WriteLine("This is the secondy dippity one.");
+                Thread.Sleep(500);
+        }
+    }
+
+    }
+
+
+  
+
+}
